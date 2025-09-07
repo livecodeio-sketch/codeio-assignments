f7de98e5cc455cb3792afb0fab1383eee1c03a62
livecodeio-sketch <livecodeio@gmail.com>
2025-09-07 22:36:58 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 18;
+        // conditional statements
+        if(age >= 18){
+            System.out.println("Watch Coolie Movie");
+        }
+        else {
+            System.out.println("You cannot watch Coolie Movie");
+        }
+    }
+}
+
+/*
+    Escape Sequence - special meaning
+* \n - new line character
+* \t - tab space
+  \\ - slash
+  \"
+  \'
+* */
